fcb8e0eb663ddacba4b9cf075a75798421aa69f4
TulasiUma <137884631+TulasiUma@users.noreply.github.com>
2023-07-19 11:05:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function bulboff(){
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR36jFbf6f_zhpfCs14OGS4XqAI7VXDTy4nhw_enemgnQ&s";
+
+}
+function bulbon(){
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXCPKLKm2sWUs5c_UN2qXNNm7hIDin5zv0Aw&usqp=CAU";
+
+}
